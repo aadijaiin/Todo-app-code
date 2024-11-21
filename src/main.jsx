9a2710b4df0fa9124d92ac2532d0fa247bd8259f
Aadi Jain <99122317+aadijaiin@users.noreply.github.com>
2024-11-21 19:08:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 import { store } from './store/store.js'
-import './index.css'
+// import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
